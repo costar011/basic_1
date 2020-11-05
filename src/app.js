fdcb1c603110c551e832ef5984373f5468227581
YERIM <dnaer456@gmail.com>
2020-11-05 21:33:23 +0900

    mongoose

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
+
+// 192.168.219.191/admin
 
 const PORT = 7000;
 
 const app = express();
 app.use(morgan(`dev`));
+
+// connect(); <--- 함수
+mongoose.connect(
+  `mongodb://4laef:fourleaf0309@192.168.219.191:27017/admin`,
+  {
+    dbName: `EDU_1`,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  (error) => {
+    if (error) {
+      console.log("Failed To DB Connect");
+    } else {
+      console.log("✅ CONNECT TO DB!");
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`${PORT} Server Start`);
